c3f859551f4f7e4b471c391064c182427412fad0
JT Bates <jtbates@gmail.com>
2011-04-08 23:36:18 -0400

    TCP connection between DirectoryClient and DirectoryServer

--- src/edu/purdue/cs252/lab6/serverapp/DirectoryServer.java
+++ src/edu/purdue/cs252/lab6/serverapp/DirectoryServer.java
@@ -1,7 +1,50 @@
 package edu.purdue.cs252.lab6.serverapp;
 
-public class DirectoryServer {
-	public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class DirectoryServer {
+	static final String SERVERIP = "127.0.0.1";
+	static final int SERVERPORT = 25201;
+	
+	public static void main(String[] args) {
+		//Logger.global.setLevel(Level.OFF);
+		
+		try {
+			// Create a socket for handling incoming requests
+			ServerSocket server = new ServerSocket(SERVERPORT);
+
+			do {
+				// Wait for an incoming connection
+				System.out.println("TCP S: Waiting for new connection...");
+				Socket clientSocket = server.accept();
+				System.out.println("TCP S: New connection received.");
+
+				// Read data from the client
+				InputStream stream = clientSocket.getInputStream();
+				// InputStream is an abstract class. We needed to use a subclass
+				BufferedReader data = new BufferedReader(new InputStreamReader(stream));
+
+				// Read a line at a time
+				String line;
+				while ((line = data.readLine()) != null) {
+					System.out.println("TCP S: Received: '" + line + "'");
+				}
+				System.out.println("TCP S: Done.");
+
+			} while (true);
+
+		} catch (IOException e) {
+			System.out.println("TCP S: Error" + e);
+		}
 	}
 }
--- src/edu/purdue/cs252/lab6/userapp/Home.java
+++ src/edu/purdue/cs252/lab6/userapp/Home.java
@@ -2,6 +2,9 @@ package edu.purdue.cs252.lab6.userapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Home extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,13 @@ public class Home extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final Button button = (Button) findViewById(R.id.ButtonConnectDS);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Start the directory client
+        		new Thread(new DirectoryClient()).start();
+            }
+        });
     }
 }
