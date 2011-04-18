01258a181e6956ec8ddddd55d1d7119610928b1c
JT Bates <jtbates@gmail.com>
2011-04-17 20:17:20 -0400

    Changes to DirectoryServer and DirectoryCommand to handle the protocol and to redirect UDP packets for the calls

--- src/edu/purdue/cs252/lab6/DirectoryCommand.java
+++ src/edu/purdue/cs252/lab6/DirectoryCommand.java
@@ -4,20 +4,31 @@ public enum DirectoryCommand {
 	// commands that the client sends to the server
 	C_LOGIN(0x10), // logs the user in; argument is the client's User class
 	C_LOGOUT(0x11), // no argument
-	C_GETDIRECTORY(0x12), // gets a listing of all logged in users; no argument
-	C_PLACECALL(0x13), // argument is the username of the call recipient (String)
-	C_ACCEPTCALL(0x14), // argument is the username of the caller (String)
+	C_DIRECTORY_GET(0x12), // requests a listing of all logged in users; no argument
+	C_CALL_ATTEMPT(0x13), // argument is the username of the call recipient (String)
+	C_CALL_ANSWER(0x14), // argument is the username of the caller (String)
+	C_CALL_HANGUP(0x15), // disconnects from the call
 	
 	// commands the server sends to a client 
-	S_INCOMINGCALL(0x20), // another user is make an incoming call; argument is the caller's username (String)
-	S_CALLACCEPTED(0x21), // the outgoing call was accepted by the recipient; no argument
-	S_REDIRECT_INIT(0x22), // specifies the port on the server the user will user for the call; argument is int
-	S_REDIRECT_READY(0x23), // tells the client that the other user is ready to receive communications 
-	S_INVALID_USER(0x24), // tells the client that the username is already taken
+	S_CALL_INCOMING(0x20), // another user is make an incoming call; argument is the caller's username (String)
+	S_CALL_ACCEPTED(0x21), // the outgoing call was accepted by the recipient; no argument
+	S_REDIRECT_INIT(0x22), // specifies the port on the server the user will user for the call; argument is an integer for the port
+	S_REDIRECT_READY(0x23), // tells the client that the other user is ready to receive communications; no argument
+	S_DIRECTORY_SEND(0x24), // sends a list of all logged in users; argument is map of users
+	S_CALL_DISCONNECT(0x25), // informs the client that the call has been ended; argument is the username of the user that disconnected
+
+	// status messages the server sends to a client
+	S_ERROR_USERALREADYEXISTS(0x30), // login failed because the username is already taken; no argument
+	S_ERROR_USERDOESNOTEXIST(0x31), // call failed because the user is not logged in; no argument
+	S_ERROR_CALLFAILED(0x32), // call failed for unknown reason
+	S_STATUS_OK(0x33), // command was successful; argument is the DirectoryCommand
+	
 	
 	// commands the server broadcasts to all clients
-	S_BC_USERLOGGEDIN(0x25), // a user logged in; argument is the user's User class
-	S_BC_USERLOGGEDOUT(0x26); // a user logged out; argument is the user's username (String)
+	S_BC_USERLOGGEDIN(0x40), // a user logged in; argument is the user's User class
+	S_BC_USERLOGGEDOUT(0x41); // a user logged out; argument is the user's username (String)
+	
+
 
 	private int code;
 	
--- src/edu/purdue/cs252/lab6/serverapp/DirectoryServer.java
+++ src/edu/purdue/cs252/lab6/serverapp/DirectoryServer.java
@@ -7,19 +7,38 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executor;
 
 import edu.purdue.cs252.lab6.DirectoryCommand;
 import edu.purdue.cs252.lab6.User;
+import edu.purdue.cs252.lab6.userapp.Call;
 
 public class DirectoryServer {
 	static final String SERVERIP = "127.0.0.1";
 	static final int SERVERPORT = 25201, MAXC = 10;
+	static Integer lastCallID = 0;
 	
-	//Userlist for storing the users logged into the directory server
-	private final ConcurrentHashMap<String, User> userList = new ConcurrentHashMap<String, User>();
+	// Map for storing the users logged into the directory server
+	private final ConcurrentHashMap<String, User> userMap = new ConcurrentHashMap<String, User>();
+	// Map for storing the client objects (which the thread processing client communications)
+	private final ConcurrentHashMap<String, Client> clientMap = new ConcurrentHashMap<String, Client>();
+	// Map for all ongoing calls
+	private final ConcurrentHashMap<String,Call> callMap = new ConcurrentHashMap<String,Call>(); 
 	
 	public static void main(String[] args) {
 		DirectoryServer dserver = new DirectoryServer();
@@ -83,58 +102,245 @@ public class DirectoryServer {
 		}	
 	}
 	
-	private class clientThread implements Runnable {
+	private class Client {
+		private String username;
 		private Socket client;
+		private ObjectInputStream ois;
+		private ObjectOutputStream oos;
+		private Thread thread;
 		
-		clientThread(Socket client) {
+		Client(User user, Socket client) throws ClassNotFoundException, StreamCorruptedException, IOException {
 			this.client = client;
-		}
-		
-		public void run() {
-			try {
-				ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
-				while(true) {
-					try {
-						DirectoryCommand command = (DirectoryCommand) ois.readObject();
-						switch(command) {
-							case C_LOGOUT:
-								
-								break;
-							case C_GETDIRECTORY:
-							
-								break;
-							case C_PLACECALL:
-								
-								break;
-							case C_ACCEPTCALL:
-								
-								break;
-							default:
-								// error, unrecognized command
-								throw new IOException("Unrecognized command: " + command.getCode());
+			this.username = user.getUserName();
+			this.ois = new ObjectInputStream(client.getInputStream());
+			this.oos = new ObjectOutputStream(client.getOutputStream());
+			thread = new Thread() {
+				public void run() {
+					while(true) {
+						try { 
+							DirectoryCommand command = (DirectoryCommand) ois.readObject();
+						
+							switch(command) {
+								case C_LOGOUT:
+									logout();
+									break;
+								case C_DIRECTORY_GET:
+									directory_send();
+									break;
+								case C_CALL_ATTEMPT:
+									call_attempt((String) ois.readObject());
+									break;
+								case C_CALL_ANSWER:
+									call_answer((String) ois.readObject());
+									break;
+								case C_CALL_HANGUP:
+									call_hangup();
+								default:
+									// error, unrecognized command
+									throw new IOException("Unrecognized command: " + command.getCode());
+							}
+						}
+						catch(IOException e) {
+							System.out.println("TCP S: Error " + e);
+							e.printStackTrace();
+						}
+						catch(ClassNotFoundException e) {
+							System.out.println("TCP S: Error " + e);
+							e.printStackTrace();					
 						}
 					}
-					catch(ClassNotFoundException e) {
-						System.out.println("TCP S: Error " + e);
-						e.printStackTrace();
-					}
+				}
+			};
+			thread.run();
+		}
+		
+		private void call_attempt(String username2) throws IOException {
+			Client client2 = clientMap.get(username2);
+			if(client2 == null) {
+				synchronized(client) {
+					oos.writeObject(DirectoryCommand.S_ERROR_USERDOESNOTEXIST);
 				}
 			}
-			catch(IOException e) {
-				System.out.println("TCP S: Error " + e);
-				e.printStackTrace();
+			else {
+				Client clientThread2 = clientMap.get(username2);
+				clientThread2.call_incoming(username);
 			}
 		}
+		
+		private void call_answer(String username2) throws IOException {
+			Client client2 = clientMap.get(username2);
+			if(client2 == null) {
+				synchronized(client) {
+					oos.writeObject(DirectoryCommand.S_ERROR_USERDOESNOTEXIST);
+				}
+			}
+			else {
+				Call call = new Call(username,username2);
+				callMap.put(username, call);
+
+				client2.call_accepted(username);
+			}
+		}
+		
+		private void directory_send() throws IOException {
+			synchronized(client) {
+				oos.writeObject(DirectoryCommand.S_DIRECTORY_SEND);
+				oos.writeObject(userMap);
+			}
+		}
+		
+		private void call_hangup() throws IOException {
+			Call call = callMap.get(username);
+			if(call == null) {
+				throw new IllegalStateException("User " + username + " attempted to disconnect from a call not found in callMap");
+			}
+			else {
+				call.disconnect(username);
+			}
+		}
+
+		private void call_incoming(String username2) throws IOException {
+			synchronized(client) {
+				oos.writeObject(DirectoryCommand.S_CALL_INCOMING);
+				oos.writeObject(username2);
+			}
+		}
+
+		private void call_accepted(String username2) throws IOException {
+			Call call = callMap.get(username2);
+			if(call == null) {
+				synchronized(client) {
+					oos.writeObject(DirectoryCommand.S_ERROR_CALLFAILED);
+				}
+			}
+			else {
+				callMap.put(username, call);
+				synchronized(client) {
+					oos.writeObject(DirectoryCommand.S_CALL_ACCEPTED);
+				}
+
+				synchronized(client) {
+					oos.writeObject(DirectoryCommand.S_REDIRECT_INIT);
+					oos.writeInt(call.getPort(username));
+				}
+			}
+		}
+		
+		private void call_disconnect(String username2) throws IOException {
+			synchronized(client) {
+				oos.writeObject(DirectoryCommand.S_CALL_DISCONNECT);
+				oos.writeObject(username2);
+			}
+		}
+
+		private void success(DirectoryCommand command) throws IOException {
+			synchronized(client) {
+				oos.writeObject(DirectoryCommand.S_STATUS_OK);
+				oos.writeObject(command);
+			}
+		}
+		
+		private void logout() throws IOException {
+			clientMap.remove(username);
+			thread.stop();
+			
+			synchronized(client) {
+				ois.close();
+				oos.close();
+				client.close();
+			}
+		}
+		
 	}
 
-	private void login(User u, Socket client) {
+	private class Call {
+		HashMap<String,Integer> idMap;
+		ArrayList<String> usernameList;
+		ArrayList<MulticastSocket> socketList;
+		ArrayList<Thread> threadList;
+		//ArrayList<InetAddress> groupList;
 		
+		Call(String username1, String username2) throws IOException {
+			connect(username1);
+			connect(username2);
+		}
+		
+		int getPort(String username) {
+			MulticastSocket redirect = socketList.get(idMap.get(username));
+			if(redirect == null) return -1;
+			else return redirect.getLocalPort();
+		}
+		
+		synchronized void connect(String username) throws IOException {
+			final int id = usernameList.size();
+			usernameList.add(username);
+			final MulticastSocket redirectFromSocket = new MulticastSocket();
+			socketList.add(redirectFromSocket);
+			//InetAddress group = InetAddress.getByName("230.0.0." + (id+1));
+			//groupList.add(group);
+			// forget multicast for now, maybe implement later
+			
+			Thread redirectThread = new Thread() {
+				public void run() {
+					int minSize = 100; // need to decide this
+					byte[] buf=new byte[minSize];
+					// might be fatally slow; we'll see
+					while (true) {
+						try {
+							DatagramPacket packet = new DatagramPacket(buf, buf.length);
+							redirectFromSocket.receive(packet);
+							for(int i=0; i<usernameList.size();i++) {
+								if(i != id) {
+									DatagramSocket redirectToSocket = socketList.get(i);
+									redirectToSocket.send(packet);
+								}
+							}
+						}
+						catch (IOException e) {
+							System.out.println("UDP S: Error" + e);
+							e.printStackTrace();
+						}
+					}
+				}
+			};
+			threadList.add(redirectThread);
+			redirectThread.start();
+		}
+		
+		synchronized void disconnect(String user_disconnecting) throws IOException {
+			int id = idMap.get(user_disconnecting);
+			MulticastSocket socket = socketList.get(id);
+			Thread thread = threadList.get(id);
+			
+			
+			for(int i=0;i<usernameList.size();i++) {
+				if(i!=id) {
+					String username = usernameList.get(i);
+					Client client = clientMap.get(username);
+					client.call_disconnect(user_disconnecting);
+				}
+			}
+			
+			socket.close();
+			thread.stop();
+
+			Client client_disconnecting = clientMap.get(user_disconnecting);
+			client_disconnecting.success(DirectoryCommand.C_CALL_HANGUP);			
+		}
+	}
+	
+	private void login(User user, Socket clientSocket) throws IOException, ClassNotFoundException {
+		String username = user.getUserName();
 		//if the user name is already taken
-		if (userList.containsKey(u.getUserName())) {
-			//Send message back to client that user name is invlaid
+		if (userMap.containsKey(username)) {
+			//Send message back to client that user name is invalid
+			ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+			oos.writeObject(DirectoryCommand.S_ERROR_USERALREADYEXISTS);
 		}
 		else {
-			
+			userMap.put(username,user);
+			Client client = new Client(user,clientSocket);
+			clientMap.put(username,client);
 		}
 		
 	}
