6daf5bed994e3c28cd569189386611bdc773120a
Paul Frederickson <pfrederi@purdue.edu>
2011-04-25 16:08:40 -0400

    Sorry if I broke the build

---
+++ src/edu/purdue/cs252/lab6/userapp/ActivitySelectUser.java
@@ -0,0 +1,12 @@
+package edu.purdue.cs252.lab6.userapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivitySelectUser extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+}
