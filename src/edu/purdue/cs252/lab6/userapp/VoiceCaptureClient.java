98195343fff28e3e90476898fc914a7259982b74
Dylan Fistrovic <Dfistrov@purdue.edu>
2011-04-25 15:41:26 -0400

    Single Buffer Attempt

--- src/edu/purdue/cs252/lab6/userapp/VoiceCaptureClient.java
+++ src/edu/purdue/cs252/lab6/userapp/VoiceCaptureClient.java
@@ -1,8 +1,10 @@
 package edu.purdue.cs252.lab6.userapp;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 import android.media.AudioFormat;
@@ -12,87 +14,56 @@ import android.util.Log;
 
 public class VoiceCaptureClient extends Thread {
 	private static final String TAG = "VCC";
-	static public DatagramSocket socket;
 	private String server;
-	private int rPort; // redirect port
+	private int port; // redirect port
 	//private final int lPort = 25202; // local port
-	
-	private int sampleRate = 8000;
+	private int bufferSize;
+	private byte[] buffer;
+
+	private int sampleRate = 11025;
 	private int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
 	private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+
 	
 	VoiceCaptureClient(String server, int port) {
 		super();
 		this.server = server;
-		this.rPort = port;
-		VoiceCaptureClient.socket = VoicePlayerServer.socket;
+		this.port = port;
 	}
 	
 	public void run() {
+		android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);		
+		 AudioRecord recorder;
+
+		// Create a new recorder
+		recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, channelConfig, audioFormat, bufferSize);
+
+		// Start the recording
+		recorder.startRecording();
 		try {
-			android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);		
-			
-			// Initialize
-			AudioRecord recorder = null;
-			byte[][] buffers = new byte[256][160];
-			int ix = 0;
-			DatagramPacket packet;
-	
-			// Retrieve the ServerName
-			final InetAddress serverAddr = InetAddress.getByName(server);
-			
-			// Minimum buffer size (can be increased later)
-			int N = AudioRecord.getMinBufferSize(sampleRate,channelConfig,audioFormat);
-	
-			// Construct instance of AudioRecord
-			recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,sampleRate,channelConfig,audioFormat,N*10);
-	
-			//Log.d("UDP","VCC: Connecting to " + usr.getUserName() + "...");
-	
-			recorder.startRecording();
-			
-			while(!isInterrupted()) {
-				try {
-					
-					byte[] buffer = buffers[ix++ % buffers.length];
-	
-					//Read data from mic into buf
-					N = recorder.read(buffer,0,buffer.length);
-					//Put buffer in a packet
-					packet=new DatagramPacket(buffer,buffer.length,serverAddr,rPort);
-	
-					//Send the packet
-					socket.send(packet);
-					
-					/*
-					// Retrieve the ServerName
-					InetAddress serverAddr = InetAddress.getByName(server);
-		
-					Log.i("UDP", "VCC: Connecting to " + server + " on " + rPort + "...");
-					// Create new UDP-Socket 
-					//DatagramSocket socket = new DatagramSocket(lPort);
-					
-					// Prepare some data to be sent. 
-					byte[] buf = ("Hello from VoiceCaptureClient").getBytes();
-		
-					// Create UDP-packet with
-					// data & destination(url+port) 
-					DatagramPacket packet = new DatagramPacket(buf, buf.length, serverAddr, rPort);
-					Log.i("UDP", "C: Sending: '" + new String(buf) + "'");
-		
-					// Send out the packet 
-					socket.send(packet);
-					Log.i("UDP", "C: Sent.");
-					Log.i("UDP", "C: Done.");
-					*/
-				} catch (Exception e) {
-					Log.e(TAG, "C: Error", e);
-				}
+			InetAddress serverAddr = InetAddress.getByName(server);
+			DatagramSocket socket = new DatagramSocket();
+
+			// Loop forever recording input
+			while (true) {
+				// Read from the microphone
+				recorder.read(buffer, 0, bufferSize);
+				DatagramPacket packet = new DatagramPacket(buffer, bufferSize, serverAddr, port);
+				socket.send(packet);
+
 			}
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		catch(UnknownHostException e) {
-			Log.e(TAG,e.toString());
-			this.interrupt();
-		}
+
+		
 	}
 }
