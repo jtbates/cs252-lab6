ffce92e4d8e964a8438465e38924c10f3150a070
Dylan Fistrovic <Dfistrov@purdue.edu>
2011-04-25 15:41:37 -0400

    Single Buffer

--- src/edu/purdue/cs252/lab6/userapp/VoicePlayerServer.java
+++ src/edu/purdue/cs252/lab6/userapp/VoicePlayerServer.java
@@ -9,71 +9,62 @@ import java.net.UnknownHostException;
 
 import android.media.AudioFormat;
 import android.media.AudioManager;
+import android.media.AudioRecord;
 import android.media.AudioTrack;
 import android.util.Log;
 
 public class VoicePlayerServer extends Thread {
 	private static final String TAG = "VPS";
-	static public DatagramSocket socket;
+	private int bufferSize;
+	private byte[] buffer;
 
-	private int sampleRate = 8000;
+	private int sampleRate = 11025;
 	private int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
 	private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+	private String server;
+	private int port;
 	
 	VoicePlayerServer(String server, int port) throws UnknownHostException, SocketException {
 		super();
-		socket = new DatagramSocket();
+		this.server = server;
+		this.port = port;
+		bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+		buffer = new byte[bufferSize];
 	}
 	
 	public void run() {
 		android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
 		AudioTrack speaker = null;
-		byte[][] buffers = new byte[256][160];
-		int ix = 0;
-		
-		// Minimum buffer size (can be increased later)
-		int N =AudioTrack.getMinBufferSize(sampleRate,channelConfig,audioFormat);
+		/**
+		 * Class that will play the sound
+		 */
 
-		// Create instance of AudioTrack
-		speaker = new AudioTrack(AudioManager.STREAM_VOICE_CALL,sampleRate,channelConfig,audioFormat,N,AudioTrack.MODE_STREAM);
+		AudioTrack player;
+		player = new AudioTrack(AudioManager.STREAM_VOICE_CALL, sampleRate, channelConfig, audioFormat, bufferSize, AudioTrack.MODE_STREAM);
+		player.play();
 
-		speaker.play();
-		
-		while(!isInterrupted()) {
-			try {
-				byte[] buffer = buffers[ix++ % buffers.length];
+		try {
+			InetAddress serverAddr = InetAddress.getByName(server);
+			DatagramSocket socket = new DatagramSocket(port, serverAddr);
+			DatagramPacket packet = new DatagramPacket(buffer, bufferSize);
 
-				//Define the packet
-				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-				// Receive the packet
-				socket.receive(packet);
-
-				// Read packet data and write to a speaker
-				buffer=packet.getData();
-				speaker.write(buffer,0,buffer.length);
-				Log.i(TAG,"speaker write");
-				/*
-				Log.i("UDP", "VPS: Connecting...");
-				// Create new UDP-Socket 
-				//DatagramSocket socket = new DatagramSocket(SERVERPORT, serverAddr);
+			// Loop forever playing the audio
+			while (true) {
 				
-				// By magic we know, how much data will be waiting for us 
-				byte[] buf = new byte[17];
-				// Prepare a UDP-Packet that can
-				// contain the data we want to receive 
-				DatagramPacket packet = new DatagramPacket(buf, buf.length);
-				Log.i("UDP", "VPS: Receiving...");
-	
-				// Receive the UDP-Packet 
 				socket.receive(packet);
-				Log.i("UDP", "VPS: Received: '" + new String(packet.getData()) + "'");
-				Log.i("UDP", "VPS: Done.");
-				*/
-				
-			} catch (IOException e) {
-				Log.e(TAG, "Error: "+ e);
+				player.write(packet.getData(), 0, bufferSize);
+			
 			}
+		
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
